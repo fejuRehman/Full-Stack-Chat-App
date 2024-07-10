c5e57b6aec1ad45a08b7a5bf29c329e3f193af8f
Feju Rehman <faiz931248@gmai.com>
2024-07-11 01:15:41 +0530

    updated

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,88 +1,102 @@
-import axios from 'axios'
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { Outlet ,useLocation,useNavigate} from 'react-router-dom'
+import axios from 'axios'
+import {useDispatch} from "react-redux"
 import { logout, setOnlineUser, setSocketConnection, setUser } from '../redux/userSlice'
 import Sidebar from '../components/Sidebar'
-import logo from '../assets/logo.png'
-import io from 'socket.io-client'
+import logo from "../assets/logo.png"
+import io from "socket.io-client"
 
 const Home = () => {
-  const user = useSelector(state => state.user)
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const location = useLocation()
+  
+ 
+  const dispatch=useDispatch()
+  const navigate=useNavigate()
+  const location=useLocation()
+//   console.log("current user details",user)
 
-  console.log('user',user)
-  const fetchUserDetails = async()=>{
-    try {
+  
+  const fetchUserDetails=async()=>{
+       try{
         const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`
-        const response = await axios({
-          url : URL,
-          withCredentials : true
+        const response=await axios({
+            url:URL,
+            withCredentials:true
         })
-
-        dispatch(setUser(response.data.data))
+        
+        dispatch(setUser(response?.data?.data))
 
         if(response.data.data.logout){
-            dispatch(logout())
-            navigate("/email")
-        }
-        console.log("current user Details",response)
-    } catch (error) {
-        console.log("error",error)
-    }
+             dispatch(logout())
+             console.log("cannot get user details " ,response?.data?.data)
+             navigate('/email')
+        } 
+
+       }
+       catch(err){
+           console.log("error" ,err);
+       }
   }
 
   useEffect(()=>{
-    fetchUserDetails()
-  },[])
+     fetchUserDetails()
+  })
 
-  /***socket connection */
-  useEffect(()=>{
-    const socketConnection = io(process.env.REACT_APP_BACKEND_URL,{
-      auth : {
-        token : localStorage.getItem('token')
-      },
-    })
+  
+  // socket connection 
 
-    socketConnection.on('onlineUser',(data)=>{
-      console.log(data)
-      dispatch(setOnlineUser(data))
-    })
+ useEffect(()=>{
+     const socketConnection=io("https://chat-app-y050.onrender.com",{
+         auth: {
+             token:localStorage.getItem('token')
+         }
+     })
+     
+     socketConnection.on('onlineUser',(data)=>{
+         console.log(data)
+         dispatch(setOnlineUser(data))
 
-    dispatch(setSocketConnection(socketConnection))
+     });
 
-    return ()=>{
-      socketConnection.disconnect()
-    }
-  },[])
+     dispatch(setSocketConnection(socketConnection))
 
 
-  const basePath = location.pathname === '/'
+
+     return ()=>{
+         
+         socketConnection.disconnect()
+     }
+ })
+
+
+
+
+   const basePath=location.pathname==='/'
+  
   return (
     <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
-        <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
+        <section className={`bg-white ${!basePath && "hidden" } lg:block`}>
            <Sidebar/>
         </section>
 
-        {/**message component**/}
-        <section className={`${basePath && "hidden"}`} >
-            <Outlet/>
+        {/* message component  */}
+        <section className={`${basePath && "hidden"}`}>
+             <Outlet/>
         </section>
 
-
-        <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex" }`}>
-            <div>
-              <img
-                src={logo}
-                width={250}
-                alt='logo'
-              />
+        <div className={`items-center justify-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex"}`}>
+            <div className=''>
+               <img src={logo}
+                width={250} 
+                alt={logo}
+                />
             </div>
-            <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+            <p className='text-lg mt-3 text-slate-500'>Select user to send message</p>
         </div>
+
     </div>
+
+
   )
 }
 
